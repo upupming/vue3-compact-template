7e383d3a38004a28e2fb35ac879a6edd14a2dd92
Yiming Li <yimingli.cs@gmail.com>
2021-11-03 16:24:47 +0800

    fix: move some style to base layer

--- src/App.vue
+++ src/App.vue
@@ -48,62 +48,61 @@ import IconGitHub from '~icons/mdi/github'
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@layer base {
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-  // less feature
-  h3 {
-    color: #42b983;
+  a {
+    text-decoration: none;
+    color: hsl(160deg 100% 37% / 100%);
+    transition: 0.4s;
+  }
+
+  a:hover {
+    background-color: hsl(160deg 100% 37% / 20%);
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    position: relative;
+    font-weight: normal;
+  }
+
+  :root {
+    --color-border: rgb(60 60 60 / 12%);
+    --section-gap: 160px;
   }
 }
 
-.nav-item {
-  @apply py-1 px-2 hover:bg-[#42b983dd] bg-[#42b983ee] active:bg-[#42b983ff] text-white rounded;
-}
+@layer components {
+  .nav-item {
+    @apply py-1 px-2 hover:bg-[#42b983dd] bg-[#42b983ee] active:bg-[#42b983ff] text-white rounded;
+  }
 
-.router-link-exact-active {
-  @apply font-bold;
-}
+  .router-link-exact-active {
+    @apply font-bold;
+  }
 
-.btn {
-  @apply px-4 py-1 rounded inline-block
-    bg-teal-600 text-white cursor-pointer
-    hover:bg-teal-700
-    disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50;
-}
+  .btn {
+    @apply px-4 py-1 rounded inline-block
+      bg-teal-600 text-white cursor-pointer
+      hover:bg-teal-700
+      disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50;
+  }
 
-.icon-btn {
-  @apply inline-block cursor-pointer select-none
-    opacity-75 transition duration-200 ease-in-out
-    hover:opacity-100 hover:text-teal-600;
+  .icon-btn {
+    @apply inline-block cursor-pointer select-none
+      opacity-75 transition duration-200 ease-in-out
+      hover:opacity-100 hover:text-teal-600;
 
-  font-size: 0.9em;
-}
-
-a {
-  text-decoration: none;
-  color: hsl(160deg 100% 37% / 100%);
-  transition: 0.4s;
-}
-
-a:hover {
-  background-color: hsl(160deg 100% 37% / 20%);
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-  position: relative;
-  font-weight: normal;
-}
-
-:root {
-  --color-border: rgb(60 60 60 / 12%);
-  --section-gap: 160px;
+    font-size: 0.9em;
+  }
 }
 
 </style>
--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -13,6 +13,28 @@
 </template>
 
 <style scoped>
+@layer base {
+  h3 {
+    font-size: 1.2rem;
+    font-weight: 500;
+    margin-bottom: 0.4rem;
+    color: #2c3e50;
+  }
+
+  i {
+    display: flex;
+    place-items: center;
+    place-content: center;
+    top: calc(50% - 25px);
+    left: -26px;
+    position: absolute;
+    border: 1px solid var(--color-border);
+    border-radius: 8px;
+    width: 50px;
+    height: 50px;
+  }
+}
+
 .item {
   display: flex;
   padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
@@ -21,26 +43,6 @@
 .details {
   flex: 1;
   margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  top: calc(50% - 25px);
-  left: -26px;
-  position: absolute;
-  border: 1px solid var(--color-border);
-  border-radius: 8px;
-  width: 50px;
-  height: 50px;
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
 }
 
 .item::before {
